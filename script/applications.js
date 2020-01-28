e68edde8d1028e83e07a110de36fc417e50b841b
Hallo89 <hallo89@protonmail.com>
2020-01-28 19:51:42 +0100

    Compute the Minesweeper pattern on the first mousedown

--- script/applications.js
+++ script/applications.js
@@ -23,6 +23,10 @@ function sweeperMouseDown(e, app) {
   const states = appStates.get(app).sweeper;
   const tilePos = getSweeperTile(this, states, e);
   drawSweeperIcon('tile-empty', states, tilePos);
+
+  if (!states.pattern) {
+    states.pattern = createSweeperPattern(states);
+  }
 }
 function sweeperMouseMove(e, app) {
   if (e.buttons == 1) {
@@ -60,4 +64,48 @@ function drawSweeperIcon(icon, states, tilePos, resetTile, ignore) {
     if (!ignore) states.activeTile = resetTile ? null : tilePos;
     ctx.drawImage(img, (tilePos.x - 1) * 16, (tilePos.y - 1) * 16);
   });
+}
+function createSweeperPattern(states) {
+  let pattern = new Array(states.tileCount.y);
+  for (let i = 0; i < states.tileCount.y; i++) {
+    pattern[i] = new Array(states.tileCount.x);
+  }
+
+  //This loop only moves on if the bomb position is unique
+  for (let i = 0; i < states.bombAmount;) {
+    const bombPos = [
+      Math.floor(Math.random() * states.tileCount.x),
+      Math.floor(Math.random() * states.tileCount.y)
+    ];
+    if (pattern[bombPos[0]][bombPos[1]] !== true) {
+      pattern[bombPos[0]][bombPos[1]] = true;
+      i++;
+    }
+  }
+
+  for (let n = 0; n < pattern.length; n++) {
+    for (let i = 0; i < pattern[n].length; i++) {
+      if (pattern[n][i] !== true) {
+        let bombCount = 0;
+        if (n !== 0) {
+          bombCount += getBombCount(pattern, n, i, -1);
+        }
+        if (n !== states.tileCount.y - 1) {
+          bombCount += getBombCount(pattern, n, i, +1);
+        }
+        bombCount += getBombCount(pattern, n, i, 0, true);
+        pattern[n][i] = bombCount;
+      }
+    }
+  }
+
+  return pattern;
+
+  function getBombCount(arr, n, i, mod, ignoreCenter) {
+    let count = 0;
+    if (i !== 0 && arr[n + mod][i - 1] === true) count++;
+    if (!ignoreCenter && arr[n + mod][i] === true) count++;
+    if (i !== states.tileCount.x - 1 && arr[n + mod][i + 1] === true) count++;
+    return count;
+  }
 }
