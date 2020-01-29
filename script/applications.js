761ac459281d8290c2bc3200294935c7e913a353
Hallo89 <hallo89@protonmail.com>
2020-01-29 18:19:23 +0100

    Organize the code

--- script/applications.js
+++ script/applications.js
@@ -44,6 +44,7 @@ function sweeperMouseUp(e, app) {
   uncoverSweeperIcon(states, tilePos);
 }
 
+// ------- General Minesweeper functions -------
 function uncoverSweeperIcon(states, tilePos, checkArea = true) {
   const tile = states.pattern[tilePos.y][tilePos.x];
   let icon;
@@ -67,6 +68,47 @@ function uncoverSweeperIcon(states, tilePos, checkArea = true) {
   }
   drawSweeperIcon(icon, states, tilePos);
   states.uncovered[tilePos.y][tilePos.x] = 1;
+}
+
+function createSweeperPattern(states, tilePos) {
+  const pattern = mapSweeperField(states);
+
+  //This loop only moves on if the bomb position is unique and if it isn't at the click position
+  for (let i = 0; i < states.bombAmount;) {
+    const bombPos = [
+      Math.floor(Math.random() * states.tileCount.x),
+      Math.floor(Math.random() * states.tileCount.y)
+    ];
+    if (pattern[bombPos[0]][bombPos[1]] === true || bombPos[0] == tilePos.x && bombPos[1] == tilePos.y) continue;
+    pattern[bombPos[0]][bombPos[1]] = true;
+    i++;
+  }
+
+  for (let n = 0; n < pattern.length; n++) {
+    for (let i = 0; i < pattern[n].length; i++) {
+      if (pattern[n][i] !== true) {
+        let bombCount = 0;
+        if (n !== 0) {
+          bombCount += getBombCount(pattern, n, i, -1);
+        }
+        if (n !== states.tileCount.y - 1) {
+          bombCount += getBombCount(pattern, n, i, +1);
+        }
+        bombCount += getBombCount(pattern, n, i, 0, true);
+        pattern[n][i] = bombCount;
+      }
+    }
+  }
+
+  return pattern;
+
+  function getBombCount(arr, n, i, mod, ignoreCenter) {
+    let count = 0;
+    if (i !== 0 && arr[n + mod][i - 1] === true) count++;
+    if (!ignoreCenter && arr[n + mod][i] === true) count++;
+    if (i !== states.tileCount.x - 1 && arr[n + mod][i + 1] === true) count++;
+    return count;
+  }
 }
 
 function findEmptySweeperArea(states, tilePos, dirMod, visited, positions = new Array()) {
@@ -127,48 +169,6 @@ function drawSweeperIcon(icon, states, tilePos, keepActive) {
     });
   }
 }
-
-function createSweeperPattern(states, tilePos) {
-  const pattern = mapSweeperField(states);
-
-  //This loop only moves on if the bomb position is unique and if it isn't at the click position
-  for (let i = 0; i < states.bombAmount;) {
-    const bombPos = [
-      Math.floor(Math.random() * states.tileCount.x),
-      Math.floor(Math.random() * states.tileCount.y)
-    ];
-    if (pattern[bombPos[0]][bombPos[1]] === true || bombPos[0] == tilePos.x && bombPos[1] == tilePos.y) continue;
-    pattern[bombPos[0]][bombPos[1]] = true;
-    i++;
-  }
-
-  for (let n = 0; n < pattern.length; n++) {
-    for (let i = 0; i < pattern[n].length; i++) {
-      if (pattern[n][i] !== true) {
-        let bombCount = 0;
-        if (n !== 0) {
-          bombCount += getBombCount(pattern, n, i, -1);
-        }
-        if (n !== states.tileCount.y - 1) {
-          bombCount += getBombCount(pattern, n, i, +1);
-        }
-        bombCount += getBombCount(pattern, n, i, 0, true);
-        pattern[n][i] = bombCount;
-      }
-    }
-  }
-
-  return pattern;
-
-  function getBombCount(arr, n, i, mod, ignoreCenter) {
-    let count = 0;
-    if (i !== 0 && arr[n + mod][i - 1] === true) count++;
-    if (!ignoreCenter && arr[n + mod][i] === true) count++;
-    if (i !== states.tileCount.x - 1 && arr[n + mod][i + 1] === true) count++;
-    return count;
-  }
-}
-
 function mapSweeperField(states, method) {
   let pattern = new Array(states.tileCount.y);
   for (let i = 0; i < states.tileCount.y; i++) {
