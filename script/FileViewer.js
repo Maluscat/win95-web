a589e902b626d203bfee3bd541ac6e5ccc18d981
Hallo89 <hallo89@protonmail.com>
2021-05-10 16:52:39 +0200

    File/Folder restructuring, now using dedicated addToDOM & open functions
    - Which makes more sense than channeling File/Folder functionality 
    through FileViewer
    - Done using an abstract parent class FileItemBase

--- script/FileViewer.js
+++ script/FileViewer.js
@@ -6,19 +6,16 @@ function FileViewer(itemTarget, path = new Array()) {
   that.path = path;
 
   that.newFolder = newFolder;
-  that.addItem = addItem;
   that.removeFileViewer = removeFileViewer;
 
-  //TODO pass a custom path
   that.directory = path.reduce((acc, curr) => {
     for (const item of acc.children) {
       if (item.name == curr) return item;
     }
   }, WinFileSystem);
 
-  // ------- Init -------
   for (const directoryChild of that.directory.children) {
-    addItem(directoryChild);
+    directoryChild.addToDOM(that.itemTarget, path);
   }
 
   let linkedViewers;
@@ -30,44 +27,13 @@ function FileViewer(itemTarget, path = new Array()) {
   }
   linkedViewers.add(that);
 
-  // ------- Class functions -------
-  function openItem() {
-    if (this.classList.contains('folder')) {
-      const itemName = this.querySelector('.text').textContent;
-      addApp('explorer', function(newApp, newStates) {
-        const newPath = Array.from(path);
-        newPath.push(itemName);
-        newStates.path = newPath;
-      });
-    } else {
-      if (this.dataset.execute) {
-        addApp(this.dataset.execute);
-      }
-    }
-  }
-
+  // ------- Prototype functions -------
   function newFolder() {
     const folder = new Folder('New folder');
     that.directory.children.push(folder);
     for (const viewer of linkedViewers) {
-      addItem(folder, viewer.itemTarget);
+      folder.addToDOM(viewer.itemTarget, path);
     }
-  }
-
-  function addItem(item, target = that.itemTarget) {
-    const node = cloneSnippet('file-item');
-    node.addEventListener('dblclick', openItem);
-    let fullName = item.name;
-    if (item instanceof Folder) {
-      node.classList.add('folder');
-    } else {
-      const internalName = item.execute || item.name;
-      node.classList.add('file');
-      node.dataset.execute = node.dataset.appIcon = internalName;
-      fullName += '.' + item.extension;
-    }
-    node.querySelector('.text').textContent = fullName;
-    target.appendChild(node);
   }
 
   function removeFileViewer() {
--- script/application/filesystem.js
+++ script/application/filesystem.js
@@ -1,16 +1,53 @@
 'use strict';
-class Folder {
-  constructor(name, children, props) {
-    this.name = name;
-    this.children = children || new Array();
+class FileItemBase {
+  constructor(props) {
     for (const key in props) this[key] = props[key];
   }
+
+  addToDOM(targetNode, path = new Array(), className, name) {
+    const node = cloneSnippet('file-item');
+    node.classList.add(className);
+    node.querySelector('.text').textContent = name;
+    node.addEventListener('dblclick', this.open.bind(this, path));
+    targetNode.appendChild(node);
+    return node;
+  }
 }
-class File {
-  constructor(name, extension, props) {
-    this.name = name;
-    this.extension = extension;
-    for (const key in props) this[key] = props[key];
+
+class Folder extends FileItemBase {
+  constructor(name, children = new Array(), props = {}) {
+    super({...props, name, children});
+  }
+
+  addToDOM(targetNode, path) {
+    super.addToDOM(targetNode, path, 'folder', this.name);
+  }
+
+  open(path) {
+    addApp('explorer', (newApp, newStates) => {
+      const newPath = Array.from(path);
+      newPath.push(this.name);
+      newStates.path = newPath;
+    });
+  }
+}
+class File extends FileItemBase {
+  constructor(name, extension, props = {}) {
+    super(Object.assign(props, {
+      name,
+      extension,
+      internalName: props.execute || name
+    }));
+  }
+
+  addToDOM(targetNode, path) {
+    const node = super.addToDOM(targetNode, path, 'file', this.name + '.' + this.extension);
+    node.dataset.appIcon = this.internalName;
+  }
+
+  open() {
+    // TODO Don't just execute everything when implementing files other than .exe
+    addApp(this.internalName);
   }
 }
 
